e00c2b28879a214a307f10e3ce9b966aad69feba
tawaridivine-droid <tawaridivine@gmail.com>
2026-05-25 10:57:27 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'ae01.alicdn.com',
+      'cjdropshipping.com',
+      'i.imgur.com',
+    ],
+  },
+}
+
+module.exports = nextConfig
